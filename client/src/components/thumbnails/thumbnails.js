dbdb67f47cc891e8c1a9b56083e770b51cadf07e
Zachary Legros <legroszach3921@gmail.com>
2019-12-03 14:04:08 -0500

    adjusted layout of cvs

--- client/src/components/thumbnails/thumbnails.js
+++ client/src/components/thumbnails/thumbnails.js
@@ -1,13 +1,77 @@
 import React from 'react';
+import Bois from './bois/Amarante.jpg';
 const uuidv4 = require('uuid/v4');
 
 function Thumbnails(props) {
-    const path = './bois/Amarante.jpg';
 
     return (
         <React.Fragment>
             <div className="material-thumbnail">
-                <img src={path} className="material-img"/>
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
+                <div className="material-desc">
+                    <span className="material-name">Amarante</span>
+                    <span className="material-origin">Amérique du Sud (Brésil)</span>
+                    <span className="material-price">$10</span>
+                </div>
+            </div>
+            <div className="material-thumbnail">
+                <img src={Bois} className="material-img"/>
                 <div className="material-desc">
                     <span className="material-name">Amarante</span>
                     <span className="material-origin">Amérique du Sud (Brésil)</span>
