f76fe4a417ad1264a7e0abcd9bc875f1deb8865a
Zachary Legros <legroszach3921@gmail.com>
2019-12-14 15:58:51 -0500

    fetched data from mongoDB and made mongoose Schema

--- api/app.js
+++ api/app.js
@@ -4,20 +4,28 @@ const logger = require("morgan");
 const uuidv4 = require("uuid/v4");
 const mongoose = require("mongoose");
 const app = express();
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("Connected to mongoDB!"))
+
+//Schemas
+
+const materialSchema = new mongoose.Schema({
+  path : String,
+  name: String,
+  origin: String,
+  price: String,
+  type: String
+});
+
+const materials = mongoose.model("materials", materialSchema);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/mats", (req, res) => {
-  mongoose
-    .connect(process.env.DB_URI)
-    .then(() => {
-      //response
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    materials.find({}).then(function (mats) {
+      res.send(mats);
+      });
 });
 
 module.exports = app;
