bfde60e880277c35c41ffa5f547065a9bd2b5841
Zachary Legros <legroszach3921@gmail.com>
2019-12-15 15:24:14 -0500

    changed thumbnail's design

--- client/src/components/spinner/spinner.js
+++ client/src/components/spinner/spinner.js
@@ -4,13 +4,13 @@ import "./spinner.css";
 const Spinner = () => {
   return (
     <React.Fragment>
-      <div class="sk-chase">
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
       </div>
     </React.Fragment>
   );
--- client/src/components/thumbnails/thumbnails.js
+++ client/src/components/thumbnails/thumbnails.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-
+const uuidv4 = require("uuid/v4");
 
 function Thumbnails(props) {
-  const [type, setType] = useState("Exotique");
+  const [type, setType] = useState("Loupe d'érable teinté double");
 
   const filter = props.mats.filter(obj => {
   return obj.type === type
@@ -13,8 +13,15 @@ function Thumbnails(props) {
     currency: 'CAD',
     minimumFractionDigits: 2
   });
-  
+
+  function favToggle(id){
+    const thumbnail = document.getElementById("#" + id);
+
+    console.log(thumbnail);
+  }
+
     useEffect(() => {
+      console.log(props.mats)
     }, [])
 
   return (
@@ -22,12 +29,24 @@ function Thumbnails(props) {
       {}
       {filter.map(material => {
         return (
-          <div className="material-thumbnail">
-            <div className="thumbnail-img-container"><a href={material.path} target="blank"><img src={material.path} className="material-img" alt="" /></a></div>
-            <div className="material-desc">
-              <span className="material-name">{material.name}</span>
-              <span className="material-origin">{material.origin}</span>
-              <span className="material-price">{formatter.format(material.price)}</span>
+          <div className="material-thumbnail" key={material._id} id={material._id}>
+            <div className="thumbnail-img-container">
+              <a className="thumbnail-img-link" href={material.path} target="blank">
+                <img src={material.path} className="material-img" alt="" />
+                <i className="fas fa-search-plus"></i>
+              </a>
+            </div>
+            <div className="material-thumbnail-content">
+              <div className="material-thumbnail-content-top">
+                <span className="material-origin"><i className="fas fa-globe-americas"/>{material.origin}</span>
+                <i className="far fa-heart" onClick={favToggle(material._id)}></i>
+              </div>
+              <div className="material-thumbnail-content-mid">
+                <span className="material-name">{material.name}</span>
+              </div>
+              <div className="material-thumbnail-content-bottom">
+                <span className="material-price">{formatter.format(material.price)}</span>
+              </div>
             </div>
           </div>
         );
