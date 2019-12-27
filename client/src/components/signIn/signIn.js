aa11eba82e9185cdee48a862e7d14eb769fbb5f2
Zachary Legros <legroszach3921@gmail.com>
2019-12-27 11:41:54 -0500

    redesigned form

--- client/src/components/signIn/signIn.js
+++ client/src/components/signIn/signIn.js
@@ -6,25 +6,38 @@ const SignIn = () => {
     <section id="sign-in-sign-up">
       <div className="signin-container">
         <span className="login-text">Se connecter</span>
-        <div className="should-signup">
-          <span>vous êtes nouveau chez crayobois ? </span>
-           <a><span className="orange">inscrivez-vous</span></a>
-        </div>
-        <form id="signin-form">
+        <form>
           <div className="input-field">
             <input
               type="email"
+              name="email"
               id="signin-email"
-              onkeyup="this.setAttribute('value', this.value);"
-              value=""
+              placeholder="Adresse e-mail"
+              autoComplete="off"
               required
             />
-            <label htmlFor="signin-email" class="label-email">
-                <span className="content-email">Adresse e-mail</span>
-            </label>
+            <i className="fas fa-envelope input-icon"></i>
           </div>
-        
+          <div className="input-field">
+            <input
+              type="password"
+              name="password"
+              id="signin-password"
+              placeholder="Mot de passe"
+              autoComplete="off"
+              required
+            />
+            <i className="fas fa-lock input-icon"></i>
+          </div>
+          <label className="remember-me">
+            <input type="checkbox" name="check" id="remember-me-check" />
+            <span class="remember-me-txt">Se souvenir de moi</span>
+          </label>
         </form>
+        <span className="form-btn">Se connecter</span>
+        <div className="form-or-container">
+          <span className="form-or">ou</span>
+        </div>
       </div>
     </section>
   );
