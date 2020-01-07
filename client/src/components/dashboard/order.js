199a006624d979d99c212d297f8f1c7bf4da677d
Zachary Legros <legroszach3921@gmail.com>
2020-01-06 23:44:53 -0500

    added calculation of total and maped items from cart

--- client/src/components/dashboard/order.js
+++ client/src/components/dashboard/order.js
@@ -8,6 +8,9 @@ const Order = props => {
   const [user, setUser] = authContext.user;
   const [cart, setCart] = useState(Object.assign([], user.shoppingCart));
   const [subTotal, setSubTotal] = useState(0);
+  const [taxes, setTaxes] = useState(0);
+  const [shipping, setShipping] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const formatter = new Intl.NumberFormat("fr-CA", {
     style: "currency",
@@ -15,22 +18,73 @@ const Order = props => {
     minimumFractionDigits: 2
   });
 
+  // get overall sub total
+  const overallSubTotal = () => {
+    let overallSubTotal = 0;
+
+    for (var i = 0; i < cart.length; i++) {
+      overallSubTotal += cart[i].subTotal;
+    }
+
+    return overallSubTotal;
+  };
+
+  // get value of taxes based on overall sub total
+  const taxCalc = () => {
+    const TPS = (5 / 100) * subTotal;
+    const TVQ = (9.975 / 100) * subTotal;
+
+    return TPS + TVQ;
+  };
+
+  const totalCalc = () => {
+    return subTotal + taxes + shipping;
+  };
+
   useEffect(() => {
     console.log(cart);
+
+    setSubTotal(overallSubTotal());
+    setTaxes(taxCalc());
+    setTotal(totalCalc());
   });
 
   return (
     <React.Fragment>
       <section className="order-section">
-        {cart.map(item => {
-          return (
-            <div key={uuidv4()} className="order-items-container">
-              <span className="order-item-text">{item.type}</span>
-              <span className="order-item-text">{`x${item.quantity}`}</span>
-              <span className="order-item-text">{formatter.format(item.subTotal)}</span>
-            </div>
-          );
-        })}
+        <div className="order-items-container">
+          {cart.map(item => {
+            return (
+              <div key={uuidv4()} className="order-item">
+                <span className="order-item-text">
+                  {(item.type === "Stylo personnalisé"
+                    ? `${item.pen[0].obj.name}, ${item.pen[1].obj.type +
+                        " " +
+                        item.pen[1].obj.color.toLowerCase()}`
+                    : "")}
+                    <span className="order-item-text item-quantity"> x{item.quantity}</span>
+                </span>
+                <span className="order-item-text right-aligned">
+                  {formatter.format(item.subTotal)}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="order-total">
+          <span className="order-item-text">
+            Sous-total: {formatter.format(subTotal)}
+          </span>
+          <span className="order-item-text">
+            Taxes (TPS et TVQ): {formatter.format(taxes)}
+          </span>
+          <span className="order-item-text">
+            Livraison: {formatter.format(shipping)}
+          </span>
+          <span className="order-item-text">
+            Total: {formatter.format(total)}
+          </span>
+        </div>
       </section>
     </React.Fragment>
   );
