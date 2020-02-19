8b863e75e9868d48ba98df9c593e8af437c1087f
Zachary Legros <legroszach3921@gmail.com>
2020-02-18 23:23:58 -0500

    added style for safari 11

--- client/src/components/dashboard/orders.js
+++ client/src/components/dashboard/orders.js
@@ -55,68 +55,71 @@ const Orders = props => {
           </span>
         </React.Fragment>
       ) : (
-        <section className="orders-section">
-          {focusedOrder ? (
-            <FocusedOrder order={focusedOrder} removeFocus={removeFocus} />
-          ) : (
-            <div className="orders">
-              {orders.map(order => {
-                return (
-                  <div
-                    className="order"
-                    onClick={() => {
-                      setFocusedOrder(order);
-                      const d = document.querySelector(".dashboard-right");
-                    }}
-                    key={order.id}
-                  >
-                    <div className="order-id-container">
-                      <span className="order-text">
-                        #{order.customId - 100000000}
-                      </span>
-                    </div>
-                    <div className="order-client-info-container">
-                      <div className="order-user">
-                        <div className="order-user-info">
+        <React.Fragment>
+          <span className="dashboard-content-header">Mes commandes</span>
+          <section className="orders-section">
+            {focusedOrder ? (
+              <FocusedOrder order={focusedOrder} removeFocus={removeFocus} />
+            ) : (
+              <div className="orders">
+                {orders.map(order => {
+                  return (
+                    <div
+                      className="order"
+                      onClick={() => {
+                        setFocusedOrder(order);
+                        const d = document.querySelector(".dashboard-right");
+                      }}
+                      key={order.id}
+                    >
+                      <div className="order-id-container">
+                        <span className="order-text">
+                          #{order.customId - 100000000}
+                        </span>
+                      </div>
+                      <div className="order-client-info-container">
+                        <div className="order-user">
+                          <div className="order-user-info">
+                            <span className="order-text">
+                              <i className="fas fa-user order-icon"></i>
+                              {`${order.payer.name.given_name} ${order.payer.name.surname}`}
+                            </span>
+                            <span className="order-text">
+                              <i className="fas fa-envelope order-icon"></i>
+                              {order.payer.email_address}
+                            </span>
+                          </div>
                           <span className="order-text">
-                            <i className="fas fa-user order-icon"></i>
-                            {`${order.payer.name.given_name} ${order.payer.name.surname}`}
-                          </span>
-                          <span className="order-text">
-                            <i className="fas fa-envelope order-icon"></i>
-                            {order.payer.email_address}
+                            <i className="far fa-calendar-alt order-icon"></i>
+                            {parseDate(order.create_time)}
                           </span>
                         </div>
-                        <span className="order-text">
-                          <i className="far fa-calendar-alt order-icon"></i>
-                          {parseDate(order.create_time)}
+                      </div>
+                      <div className="order-info-container">
+                        <div className="order-info-quantities">
+                          <span className="order-text">
+                            {priceFormatter.format(
+                              order.purchase_units[0].amount.value
+                            )}
+                          </span>
+                          <span className="order-text">
+                            {order.purchase_units[0].items.length}
+                            <i className="fas fa-pen-alt order-icon order-icon-right"></i>
+                          </span>
+                        </div>
+                        <span className="order-text order-state">
+                          {order.order_status === "Livré"
+                            ? "Livré"
+                            : "En attente"}
                         </span>
                       </div>
                     </div>
-                    <div className="order-info-container">
-                      <div className="order-info-quantities">
-                        <span className="order-text">
-                          {priceFormatter.format(
-                            order.purchase_units[0].amount.value
-                          )}
-                        </span>
-                        <span className="order-text">
-                          {order.purchase_units[0].items.length}
-                          <i className="fas fa-pen-alt order-icon order-icon-right"></i>
-                        </span>
-                      </div>
-                      <span className="order-text order-state">
-                        {order.order_status === "Livré"
-                          ? "Livré"
-                          : "En attente"}
-                      </span>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </section>
+                  );
+                })}
+              </div>
+            )}
+          </section>
+        </React.Fragment>
       )}
     </React.Fragment>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+/*let hacks = require('viewport-units-buggyfill/viewport-units-buggyfill.hacks');
+require('viewport-units-buggyfill').init({
+  hacks: hacks
+});*/
 
 ReactDOM.render(<App />, document.getElementById("root"));
